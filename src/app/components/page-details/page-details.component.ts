6ae5df49d0f642a558fcbd96c87984e33fba7eeb
ba-mirza <almas1610@gmail.com>
2022-02-25 10:45:38 +0600

    We get specific data from each card without conditional statements inside the pipe.

--- src/app/components/page-details/page-details.component.ts
+++ src/app/components/page-details/page-details.component.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { forkJoin, map } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 import { DbService } from 'src/app/db.service';
 
 @Component({
@@ -13,7 +14,10 @@ export class PageDetailsComponent implements OnInit {
   @Input()
   uid!: string | null
 
-  characters: any = null;
+  public characters: any;
+  public starships: any;
+  public planets: any;
+
   loading: boolean = false;
 
   constructor(
@@ -24,34 +28,34 @@ export class PageDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params: Params) => {
       this.uid = params['id'];
-      console.log(this.uid)
     });
 
     this.loading = true;
 
-    this.dbService.getAllOfData().pipe(
-      map(data => {
-       let results = null;
-       data.result?.forEach((item: any) => {
-         if(item.uid === this.uid){
-           let characters = null;
-           results = item.properties.characters;
-           results.forEach((el: string) => {
-             this.dbService.getChooseData(el).subscribe((characters) => {
-              //  console.log(characters.result);
-               characters = characters.result;
-               console.log(characters);
-             });
-           })
-         }
-       });
-       return results;
+    this.dbService.getAllOfData(this.uid).pipe(
+      map((obj: any) => {
+        let characters = obj.result.properties.characters;
+        let starships = obj.result.properties.starships;
+        let planets = obj.result.properties.planets;
+        return [characters, starships, planets];
       })
     ).subscribe(data => {
-      this.loading = false;
-      this.characters = data;
-      // console.log(this.characters);
+      this.characters = data[0];
+      this.starships = data[1];
+      // this.planets = data[2];
+      // console.log(this.planets);
+      this._methodFor(data[2]);
     });
 
+    // this.dbService.getCharacters(this.characters).subscribe(next => console.log(next));
+
+
   }
+
+  _methodFor(arg: Array<string>) {
+    for(let links of arg) {
+      this.dbService.getPlanets(links).subscribe(next => console.log(next));
+    }
+  }
+
 }
--- src/app/db.service.ts
+++ src/app/db.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { forkJoin, Observable } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,11 +10,27 @@ export class DbService {
 
   constructor(private http: HttpClient) {}
 
-  getAllOfData(): Observable<any> {
-    return this.http.get(this.dbUrl + '/films');
+  getAllOfData(id?: any): Observable<any> {
+    return this.http.get(this.dbUrl + `/films/${id}`);
   }
 
-  getChooseData(url: string): Observable<any> {
+  // getChooseData(people: any, starships: any, planets: any): Observable<any> {
+  //   return this.http.get(people);
+  //   return this.http.get(starships);
+  //   return this.http.get(people);
+  // }
+
+  getCharacters(url: string): Observable<any> {
     return this.http.get(url);
   }
+
+  getStarships(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+  getPlanets(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+
 }
